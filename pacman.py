5a0e6b43cbc8a3d318f120bee2b85682ab87a60f
zLuis <a01705457@itesm.mx>
2024-05-09 17:14:01 -0600

    Apply PEP8 to pacman from freegames

--- pacman.py
+++ pacman.py
@@ -10,7 +10,9 @@ Exercises
 """
 
 from random import choice
-from turtle import *
+from turtle import \
+    (bgcolor, clear, dot, done, goto, hideturtle, listen, onkey,
+        ontimer, Turtle, setup, tracer, up, update)
 
 from freegames import floor, vector
 
